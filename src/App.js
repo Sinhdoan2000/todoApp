200602c114d4866a62ce28d45b67ada0674ebc1d
sinhdoan2000 <sinhdoan2000ss@gmail.com>
2022-06-02 16:08:34 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,246 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import { useState, useEffect } from 'react';
-
-const tabs = [
-  "posts", "comments", "users", "todos", "photos", "albums"
-]
-
+import '@shopify/polaris/build/esm/styles.css';
+import {Button} from '@shopify/polaris';
 function App() {
+
+  const jobs = [
+      {
+        id: 0,
+        checked: false,
+        name: "clear table"
+      },
+      {
+        id: 1,
+        checked: false,
+        name: "washes my clothes"
+      },
+      {
+        id: 2,
+        checked: false,
+        name: "washes my face"
+      }
+  ]
   
-  // const [title, setTitle] = useState('');
-  // const [height, setHeight] = useState(false);
-  // const [domain, setDomain] = useState('posts');
-  // const [data, setData] = useState([]);
-  // const [avatar, setAvatar] = useState()
-  //   //TH1: useEffect với callback  => useEffect(callback);
-  //   useEffect(()=>{
-  //     window.document.title = title;
-  //     console.log('re-render - TH1: useEffect(callback)');
-  //   }) 
+  const filters = [
+      {
+        name: "All"
+      },
+      {
+        name: "Active"
+      },
+      {
+        name: "Completed"
+      }
+  ];
+  const [dataJobs, setDataJob] = useState(()=>{
+      window.localStorage.setItem('jobs', JSON.stringify(jobs));
+      const getJsons = window.localStorage.getItem('jobs');
+      const dataJSON = JSON.parse(getJsons); 
+    return dataJSON ? dataJSON : [];
+  });
 
-  //   //TH2: useEffect với callback && Array rỗng => useEffect(callback, []);
-  //   useEffect(()=>{   
-  //     console.log('re-render - TH2: useEffect(callback, [])');   
-  //     function handleScroll(){
-      
-  //       if(window.scrollY > 500){
-  //         setHeight(true);
-  //       }else{
-  //         setHeight(false)
-  //       }
-  //       console.log('re-render - TH2: useEffect(callback, [])');
-  //     }
-  //     window.addEventListener('scroll', handleScroll);
-      
-  //   }, [])
-  //   function handleGoToTop(e){
-    
-  //     // use jquery magic to get there ;) <br />
-  
-  //     window.scrollTo(0, 0);
-  //   }
+  useEffect(()=>{
+    setDataJob(dataJobs);
+  }, [dataJobs.length])
 
+  const [tabs, setTabs] = useState('All');
+  const [isSearch, setSearch] = useState(false);
+  const [addValue, setAddValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [isRender, setIsRender] = useState(false);
 
-  //   //TH3: useEffect với callback, Array chứa Dependency => useEffect(callback, [Des]);
-  //   useEffect(()=>{
-  //     console.log('re-render - TH3: useEffect(callback, [Des])');
-  //     fetch(`https://jsonplaceholder.typicode.com/${domain}`)
-  //       .then(res=> res.json())
-  //       .then(data=> setData(data))
-  //   }, [domain])
-  
-
-  //   {/* Reduce function */}
-  //   useEffect(()=>{
-  //     const data = [5, 10, 15, 20, 25];
-
-  //     const res = data.reduce((total,currentValue) => {
-  //       return total + currentValue;
-  //     }, 10);
-
-  //     console.log(res); // 75
-  //   }, [])
-    
-  //   useEffect(() => {
-  //       return () =>{
-  //         avatar && URL.revokeObjectURL(avatar.preview)
-  //       }
-  //   }, [avatar])
-    
-  //   const handlePreviewAvatar = (e) =>{
-  //     const file = e.target.files[0];
-  //     file.preview = URL.createObjectURL(file);
-  //     setAvatar(file);
-  //   }
-  //  const styleButton = {
-  //   background: 'red',
-  //   color: 'white',
-  //   padding: '20px 0px',
-  //   position: 'fixed',
-  //   right: '20px',
-  //   bottom: '20px',
-  //   border: 'none',
-  //   borderRadius: '50%',
-  //   boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-  //   cursor: 'pointer'
-  // }
-  // const styleTabButton = {background: 'black', color: 'white', margin: "0 10px", padding: "14px 20px"}
-
-  // const renderData = (
-  //   <div>
-  //      <label>Enter your text</label>
-  //       <input onChange={(e)=> setTitle(e.target.value)} />
-  //       <label>Choose Avatar</label>
-  //     <input type="file" onChange={handlePreviewAvatar} />
-
-  //     {avatar && <img src={avatar.preview} alt="" width="200px" height="200px" />} 
-
-  //       {/* Render tab map function */}
-  //       {tabs.map(function(tab, index){
-  //         return <button
-  //                   key={index} 
-  //                   style={domain == tab ? styleTabButton : {margin: "0 10px", padding: "14px 20px"}}
-  //                   onClick={()=> setDomain(tab)}
-  //                 >
-  //                   {tab}
-  //                 </button>
-  //       })}
-
-  //       {/* Render content */}
-  //       <div className="listItem" style={{marginLeft: '20px'}}>
-  //         {data.map(function(item, index){
-  //           return <div style={{textAlign: "left"}} key={index}>{index}. {item.title || item.name}</div> 
-  //         })}
-  //       </div>
-        
-  //       {/* show button Go to top */}
-  //       {height && <button 
-  //        style={styleButton}
-  //       onClick={handleGoToTop}
-  //       >
-  //           Go to top
-  //         </button>}
-  //   </div>
-  // )
-  const jobs = [];
-  const [job, setJob] = useState('');
-  const [dataJobs, setDataJobs] = useState(jobs);
-  const [reload, setReload] = useState(false);
-  const [valueEdit, setValueEdit] = useState('');
-  const $ = document.getElementById.bind(document);
-
-  const handleAddJob = (e)=>{
-    let errorMessage = document.getElementById('errorMessage');
-    //kiểm tra giá trị đã tồn tại chưa ?
-    let isExist = dataJobs.filter(function(item){
-      return item.name == job;
-    })
-
-      if((isExist.length <= 0 && job) || (e.keyCode == 13 && job && isExist.length <= 0)){
-        const newData = {
-          name: job
+  const currentData = ()=>{
+    const getJsons = window.localStorage.getItem('jobs');
+    const dataJSON = JSON.parse(getJsons); 
+    setDataJob(dataJSON);
+  }
+ //done
+  const handleAdd = (e)=>{
+      const isExist = dataJobs.filter(item=>{
+        return addValue == item.name;
+      })
+      if(isExist.length <= 0 && addValue && e.keyCode == 0){
+        let id = dataJobs.length > 0 ? dataJobs[dataJobs.length - 1].id + 1 : 0;
+        const newJob = {
+          id,
+          name: addValue
         }
-        dataJobs.push(newData);
-        setDataJobs(dataJobs);  
-        setJob("");
-        setReload(reload => !reload)
-      }else if(job == ""){
-          return false;
-      }else{
-        errorMessage.style.display = 'block';
+        dataJobs.push(newJob);
+        setAddValue("");
+        window.localStorage.setItem('jobs', JSON.stringify(dataJobs));
+        currentData();
+        setIsRender(!isRender);
       }
   }
-  const handleDeleteJob = (id)=>{
-      dataJobs.splice(id, 1);
-      setDataJobs(dataJobs);
-      setReload(reload => !reload)
+ //done
+  const handleSearch = (e) =>{
+      setSearchValue(e.target.value);
+      const getJsons = window.localStorage.getItem('jobs');
+      const dataJSON = JSON.parse(getJsons); 
+      const resultFilter = dataJSON.filter(item=>{
+        return item.name.toLowerCase().search(e.target.value.toLowerCase()) != -1; 
+      }) 
+      if(resultFilter && searchValue){
+        setDataJob(resultFilter);
+      }else if(searchValue.length == 0){
+        currentData();
+      }else{
+        setDataJob([]);
+      }
   }
-  const handleEditJob = (id, value)=>{
-      let currentElement = $(`edit-${id}`);
-      currentElement.style.display = "block";
 
-      let inputEdit = $(`edit-value-${id}`);
-      inputEdit.value = value;
+  const handleFilterTabs = (name, id)=>{
+      setTabs(name);  
+      if(name == "All"){
+        currentData();
+        const CheckedElement = document.querySelectorAll('.Globo-checkboxJob');
+        CheckedElement.forEach(item=>{
+              item.closest('.Globo-job_item').style.display = "flex";
+        })
+      }else if(name == "Active"){
+        const notCheckedElement = document.querySelectorAll('.Globo-checkboxJob');
+        notCheckedElement.forEach(item=>{
+          if (item.checked) {
+            item.closest('.Globo-job_item').style.display = "none";
+           }else{
+            item.closest('.Globo-job_item').style.display = "flex";
+          }
+        })
+
+      }else{
+        const notCheckedElement = document.querySelectorAll('.Globo-checkboxJob');
+        notCheckedElement.forEach(item=>{
+          if (!item.checked) {
+            item.closest('.Globo-job_item').style.display = "none";
+          }else{
+            item.closest('.Globo-job_item').style.display = "flex";
+          }
+        })
+      }
   }
-  const handlFinish = (id)=>{
-      let currentElement = $(`doneJob-${id}`);
-      currentElement.style.display = 'inline';
+  const handleIsChecked = (e, job) =>{   
+      e.target.checked ? job.checked = true : job.checked = false;   
+      setDataJob(dataJobs);
+      setIsRender(!isRender);
+      window.localStorage.setItem('jobs', JSON.stringify(dataJobs));
   }
-  const  handlUnFinished = (id) => {
-      let currentElement = $(`doneJob-${id}`);
-      currentElement.style.display = 'none';
-  }
-  const handleChangeValueEdit = (id)=>{ 
-      let inputEdit = $(`edit-value-${id}`);
-        ;  
-      dataJobs[id].name = inputEdit.value;
-      setDataJobs(dataJobs);
-      let currentElement = $(`edit-${id}`);
-      currentElement.style.display = "none";
-      setReload(reload => !reload)
-  }
-  const handleCloseEdit = (id)=>{
-      let currentElement = $(`edit-${id}`);
-      currentElement.style.display = "none";
-      setReload(reload => !reload)
-  }
-  const handleChangeValueAdd = (data)=>{
-      setJob(data);
-      let errorMessage = $('errorMessage');
-      errorMessage.style.display = 'none';
-  }
+
   return (
-    <div className="App">
-       {/* {renderData ? renderData : ""} */}
-      <div className="appTodo">
-        <div className="todoNav">
-            <h1>List jobs</h1>
-            <div className="todoButton">
-              <label htmlFor="#todoInput">
-                <input type="text" value={job} onChange={e => handleChangeValueAdd(e.target.value)} onKeyPress={(e) => handleAddJob(e)} placeholder="Enter your new job..."/>
-                <button className="todoAddButton add" onClick={handleAddJob} >Add new job.</button>
-                <button onClick={()=> setJob('')}>Clear</button>
-                <p id="errorMessage" style={{color: 'red', display: 'none'}}>This job has been exists in your list job!!!</p>
-              </label>
+          <div className="App">
+            <div className="Globo-app">
+              <header>
+                  <h1 className="Globo-title">Things To Do</h1>
+                  <div className="Globo-input">
+                    {!isSearch && <input type="text" 
+                        id="Globo-input_add"
+                        placeholder='Add New' 
+                        value={addValue} 
+                        onChange={e => setAddValue(e.target.value)}
+                        onKeyPress={e => handleAdd(e)}
+                    />}
+                    {isSearch && <input type="text" 
+                        id="Globo-input_search" 
+                        placeholder='Search' 
+                        onChange={e => handleSearch(e)}
+                    />}
+                  </div>
+              </header>
+              <div className="Globo-content">
+                <ul className="Globo-listJobs">
+                  {dataJobs.length > 0 ? dataJobs.map((job, index)=>{               
+                    return (
+                        <li key={index} id={"Globo-job-" + job.id} className="Globo-job_item" style={{display: "flex"}}>
+                          <input type="checkbox" 
+                            id={"Globo-checkbox-" + job.id}
+                            className="Globo-checkboxJob" 
+                            defaultChecked={job.checked}
+                            value={job.name}
+                            onClick={e => handleIsChecked(e, job)}
+                          />
+                          <label htmlFor={"Globo-checkbox-" + job.id} className="Globo-job_name">{job.name}</label>
+                        </li>
+                    )
+                  }) :  <p style={{margin: "10px 0", padding: "10px", background: "#F2F2F2", color: "#888888", fontWeight: "500"}}>There are not items</p>}
+                </ul>
+              </div>
+              <footer style={{display: "flex"}}>
+                <div className="Globo-handle" style={{display: "flex"}}>
+                  <button id="Globo-button-add" onClick={()=> setSearch(isSearch => !isSearch)}>
+                    <i className="fa-solid fa-plus"></i>
+                  </button>
+                  <button id="Globo-button-search" style={{color: "#777"}} onClick={()=> setSearch(isSearch => !isSearch)}>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                  </button>
+                  <span className="Globo-countJob">{dataJobs.length > 1 ? dataJobs.length + " items left" : dataJobs.length + " item left"}</span>
+                </div>
+                <div className='Globo-filters' style={{display: "flex"}}>
+                    {filters.map((filter, index)=>{
+                      return (
+                        <button 
+                        key={index}
+                        className='Globo-filters_button' 
+                        style={tabs == filter.name ? {border: "1px solid rgba(175, 47, 47, 0.2)"} : {}}
+                        onClick={()=> handleFilterTabs(filter.name, filter.id)}
+                        >
+                          {filter.name}
+                        </button>
+                      )
+                    })}
+                </div>
+              </footer>
             </div>
-        </div>
-        <ul className="todoList">
-            {dataJobs.map(function (item, index){
-              return <li key={index} id={index}>
-                      
-                        <div style={{display: 'flex', alignItems: 'center'}}>
-                          <div id={"doneJob-" + index} className="add doneJob" style={{ display: 'none'}}>Done</div>
-                          <p className="lineItem finishElement">{item.name}</p> 
-                        </div>
-                        <br/>                     
-                        { 
-                        <div id={"edit-" + index} className="editJobs" style={{ display: 'none'}}>
-
-                            <input id={"edit-value-" + index} type="text" onChange={e => setValueEdit(e.target.value)} />
-
-                            <button className="add"   onClick={() => handleChangeValueEdit(index)}>Save</button>
-
-                            <button className="close" onClick={() => handleCloseEdit(index)}>X</button>
-
-                        </div>
-                      }     
-                        <br/>
-
-                        <button className="delete"   onClick={()=> handleDeleteJob(index)}>Delete Job</button>
-
-                        <button className="edit"     onClick={()=> handleEditJob(index, item.name)}>Edit</button>
-
-                        <button className="finish"   onClick={()=> handlFinish(index)}>Finish</button>
-
-                        <button className="unFinish" onClick={()=> handlUnFinished(index)}>unfinished</button>
-
-                                
-                    </li>
-            })}
-        </ul>
-        </div>
-    </div>
+              <p className="message-notify">Press `Esc` to cancel.</p>
+          </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {AppProvider} from "@shopify/polaris";
+import en from '@shopify/polaris/locales/en.json';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider i18n={en}>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
 
